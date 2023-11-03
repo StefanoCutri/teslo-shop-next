18493e57d672846d72a7a3a9fd43d8fe99959f99
Stefano Cutri <stefanocutristecco@gmail.com>
2023-11-03 16:40:36 -0300

    Add CssBasline

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import "@/styles/globals.css";
 import { lightTheme } from "@/themes";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={lightTheme}>
-      <Component {...pageProps} />
+      <CssBaseline>
+        <Component {...pageProps} />
+      </CssBaseline>
     </ThemeProvider>
   );
 }
