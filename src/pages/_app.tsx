d113d065ed5232efd92b7e1493e4de6412aecb49
Stefano Cutri <stefanocutristecco@gmail.com>
2023-11-03 16:40:05 -0300

    Styles and themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { lightTheme } from "@/themes";
+import { ThemeProvider } from "@mui/material";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
