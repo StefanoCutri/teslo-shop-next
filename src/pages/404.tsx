92ead5a2043d5f41197875d8722a304393b1b5d3
Stefano Cutri <stefanocutristecco@gmail.com>
2023-11-06 12:49:35 -0300

    404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,22 @@
+import { Box, Typography } from "@mui/material";
+import ShopLayout from "@/components/layouts/ShopLayout";
+
+const Custom404 = () => {
+  return (
+    <ShopLayout title="Page not found" pageDescription="Nothing to show here">
+      <Box
+        display="flex"
+        flexDirection={{xs: 'column', sm: 'row'}}
+        justifyContent="center"
+        alignItems="center"
+        height="calc(100vh - 200px)"
+        
+      >
+        <Typography variant="h1" fontSize={80} fontWeight={200}>404 | </Typography>
+        <Typography marginLeft={2}>Sorry, the page you were looking for cannot be found</Typography>
+      </Box>
+    </ShopLayout>
+  );
+};
+
+export default Custom404;
