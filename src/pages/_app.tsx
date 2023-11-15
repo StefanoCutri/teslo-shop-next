329d063443b87fe1c2e191a2eec7de0d4280b1c8
Stefano Cutri <stefanocutristecco@gmail.com>
2023-11-15 18:31:32 -0300

    Cart context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
-import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+
+import type { AppProps } from "next/app";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { SWRConfig } from "swr";
+
 import { lightTheme } from "@/themes";
-import { UiProvider } from "@/context";
+import { CartProvider, UiProvider } from "@/context";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -13,13 +15,15 @@ export default function App({ Component, pageProps }: AppProps) {
           fetch(resource, init).then((res) => res.json()),
       }}
     >
-      <UiProvider>
-        <ThemeProvider theme={lightTheme}>
-          <CssBaseline>
-            <Component {...pageProps} />
-          </CssBaseline>
-        </ThemeProvider>
-      </UiProvider>
+      <CartProvider>
+        <UiProvider>
+          <ThemeProvider theme={lightTheme}>
+            <CssBaseline>
+              <Component {...pageProps} />
+            </CssBaseline>
+          </ThemeProvider>
+        </UiProvider>
+      </CartProvider>
     </SWRConfig>
   );
 }
