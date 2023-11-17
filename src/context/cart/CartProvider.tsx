6c00ef6f0159a6a69db7b3a1797b2ca5896427cf
Stefano Cutri <stefanocutristecco@gmail.com>
2023-11-17 13:14:34 -0300

    Save cart to cookies

--- src/context/cart/CartProvider.tsx
+++ src/context/cart/CartProvider.tsx
@@ -1,4 +1,5 @@
-import React, { FC, useReducer } from "react";
+import React, { FC, useEffect, useReducer } from "react";
+import Cookie from "js-cookie";
 import { CartContext, cartReducer } from "./";
 import { ICartProduct } from "@/interfaces";
 
@@ -6,8 +7,8 @@ export interface CartState {
   cart: ICartProduct[];
 }
 
-const CART_INITIAL_STATE: CartState = {
-  cart: [],
+export const CART_INITIAL_STATE: CartState = {
+  cart: Cookie.get("cart") ? JSON.parse(Cookie.get("cart")!) : [],
 };
 
 interface Props {
@@ -17,23 +18,54 @@ interface Props {
 export const CartProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(cartReducer, CART_INITIAL_STATE);
 
+  useEffect(() => {
+    try {
+      let cookieProducts = Cookie.get("cart")
+        ? JSON.parse(Cookie.get("cart")!)
+        : [];
+
+      dispatch({
+        type: "[Cart] - LoadCart from cookies | storage",
+        payload: cookieProducts,
+      });
+    } catch (error) {
+      dispatch({
+        type: "[Cart] - LoadCart from cookies | storage",
+        payload: [],
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    Cookie.set("cart", JSON.stringify(state.cart));
+  }, [state.cart]);
+
   const addCartToProduct = (product: ICartProduct) => {
     const productInCart = state.cart.some((p) => p._id === product._id);
-    if (!productInCart) return dispatch({ type: "[Cart] - Add Product", payload: [...state.cart, product] });
+    if (!productInCart)
+      return dispatch({
+        type: "[Cart] - Add Product",
+        payload: [...state.cart, product],
+      });
 
-    const sameProductDifferentSizes = state.cart.some((p) => p.size === product.size);
-    if(!sameProductDifferentSizes) return dispatch({ type: "[Cart] - Add Product", payload: [...state.cart, product] });
+    const sameProductDifferentSizes = state.cart.some(
+      (p) => p.size === product.size
+    );
+    if (!sameProductDifferentSizes)
+      return dispatch({
+        type: "[Cart] - Add Product",
+        payload: [...state.cart, product],
+      });
 
-    const updatedProducts = state.cart.map(p => {
+    const updatedProducts = state.cart.map((p) => {
       if (p._id != product._id) return p;
       if (p.size != product.size) return p;
 
       p.quantity += product.quantity;
-      return p
+      return p;
     });
 
     dispatch({ type: "[Cart] - Add Product", payload: updatedProducts });
-
   };
 
   return (
